2208680ae1a166f1097ef2acd5f58b9b1aed689c
mdadilraza <mdadilraza15@gmail.com>
2024-07-10 11:29:13 +0530

    first commit

---
+++ consume-soap-api/src/main/java/com/eidiko/consume_soap_api/ConsumeSoapApiApplication.java
@@ -0,0 +1,13 @@
+package com.eidiko.consume_soap_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeSoapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeSoapApiApplication.class, args);
+	}
+
+}
